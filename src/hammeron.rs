88e437c13792a8793cc5552cafcbc7982dca0386
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-06-09 02:53:34 -0400

    Update hammeron.rs

--- src/hammeron.rs
+++ src/hammeron.rs
@@ -2,6 +2,7 @@ use aes::Aes128;
 use block_modes::{BlockMode, Cbc};
 use block_modes::block_padding::Pkcs7;
 use std::env;
+use zeroize::Zeroize;
 
 type Aes128Cbc = Cbc<Aes128, Pkcs7>;
 
@@ -14,11 +15,12 @@ fn main() {
     let messbind = String::from(smessage);
     let plaintext = messbind.as_bytes();
     let key = args[3].clone();
-    let skey = hex::decode(key).expect("Failed to decode private key provided!");
+    let mut skey = hex::decode(key).expect("Failed to decode private key provided!");
     let cipher = Aes128Cbc::new_from_slices(&skey, &iv).unwrap();
     let pos = plaintext.len();
     let mut buffer = [0u8; 128];
     buffer[..pos].copy_from_slice(plaintext);
     let ciphertext = cipher.encrypt(&mut buffer, pos).unwrap();
     println!("\n{:?}",hex::encode(ciphertext));
+    skey.zeroize();
 }
