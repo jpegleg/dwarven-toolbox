3528530bc5721bd79ed553860ee6c6743663de51
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-06-08 23:09:31 -0400

    Create anvil.rs

---
+++ src/anvil.rs
@@ -0,0 +1,45 @@
+use rand::{Rng, SeedableRng};
+use rand::rngs::StdRng;
+use rand::distributions::Uniform;
+use std::iter;
+
+fn geniv() -> String {
+    let mut rng = StdRng::from_entropy();
+    let hex_chars: String = iter::repeat(())
+        .map(|()| {
+            let char_range = Uniform::from(0..16);
+            let value = match rng.sample(char_range) {
+                0..=9 => (b'0' + rng.sample(Uniform::from(0..10))) as char,
+                10..=15 => (b'A' + rng.sample(Uniform::from(0..6))) as char,
+                _ => unreachable!(),
+            };
+            value
+        })
+        .take(16)
+        .collect();
+    hex_chars
+}
+
+fn genkey() -> String {
+    let mut rng = StdRng::from_entropy();
+    let hex_chars: String = iter::repeat(())
+        .map(|()| {
+            let char_range = Uniform::from(0..16);
+            let value = match rng.sample(char_range) {
+                0..=9 => (b'0' + rng.sample(Uniform::from(0..10))) as char,
+                10..=15 => (b'A' + rng.sample(Uniform::from(0..6))) as char,
+                _ => unreachable!(),
+            };
+            value
+        })
+        .take(32)
+        .collect();
+    hex_chars
+}
+
+fn main() {
+    let iv = geniv();
+    println!("IV: {}", iv);
+    let key = genkey();
+    println!("KEY: {}", key);
+}
