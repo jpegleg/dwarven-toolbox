ac90de3ee281bdfa7463a4b5c811a79cdae7dd3b
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-06-09 01:47:12 -0400

    Create silver.rs

---
+++ src/silver.rs
@@ -0,0 +1,20 @@
+use rand::{Rng, SeedableRng};
+use rand::rngs::StdRng;
+use rand::distributions::Uniform;
+use std::iter;
+
+fn silver() {
+    let mut rng = StdRng::from_entropy();
+    let silverchar: String = iter::repeat(())
+        .map(|()| {
+            let char_range = Uniform::from(32..127);
+            rng.sample(char_range) as u8 as char
+        })
+        .take(64)
+        .collect();
+    println!("{}", silverchar);
+}
+
+fn main() {
+    silver();
+}
