74fb43e5a0ea7c4e642fdd98e3ea788a25dfc7e3
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-06-09 02:52:21 -0400

    Update iron.rs

--- src/iron.rs
+++ src/iron.rs
@@ -2,17 +2,19 @@ use rand::{Rng, SeedableRng};
 use rand::rngs::StdRng;
 use rand::distributions::Uniform;
 use std::iter;
+use zeroize::Zeroize;
 
 fn iron() {
     let mut rng = StdRng::from_entropy();
-    let ironchar: String = iter::repeat(())
+    let mut ironchar: String = iter::repeat(())
         .map(|()| {
             let char_range = Uniform::from(32..127);
             rng.sample(char_range) as u8 as char
         })
         .take(32)
         .collect();
-    println!("{}", ironchar);
+    println!("{}", &ironchar);
+    ironchar.zeroize();
 }
 
 fn main() {
