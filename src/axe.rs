8b11b8499793126562a6302587afa72a49859987
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-06-11 17:01:00 -0400

    Create axe.rs

---
+++ src/axe.rs
@@ -0,0 +1,29 @@
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+      
+    if args.len() != 3 {
+        println!("Usage: axe <number1> <number2>");
+        return;
+    }
+      
+    let number1: f64 = match args[1].parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Error: First argument must be a valid f64 number");
+            return;
+        }
+    };
+
+    let number2: f64 = match args[2].parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Error: Second argument must be a valid f64 number");
+            return;
+        }
+    };
+
+    let result = number1 / number2;
+    println!("{}", result);
+}
