3d21cbd2ec90a3ad4e313463c9a7da4ebd3d6b65
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-06-11 15:16:53 -0400

    Create swordleft.rs

---
+++ src/swordleft.rs
@@ -0,0 +1,27 @@
+fn slash(string: String) -> String {
+    let bytes = hex::decode(string).unwrap();
+    let result_bytes: Vec<u8> = bytes
+        .iter()
+        .zip(bytes.iter())
+        .map(|(&bytes, _self)| bytes << 1)
+        .collect();
+    let hex_string = hex::encode(result_bytes);
+   
+    hex_string
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: swordleft <hex encoded data string>");
+        return;
+    }
+    if let Some(arg) = std::env::args().nth(1) {
+        if let Ok(hexdata) = arg.parse::<String>() {
+            let sword = slash(hexdata);
+            println!("{}", sword);
+        } else {
+             println!("Invalid argument! The only argument is a hex encoded value to operate on.")
+        }
+    } 
+}
