89feed8c94b3291303bd0f99ef0e1ebc77397a89
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-06-10 20:04:46 -0400

    Update hammeron.rs

--- src/hammeron.rs
+++ src/hammeron.rs
@@ -8,6 +8,10 @@ type Aes128Cbc = Cbc<Aes128, Pkcs7>;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.len() != 4 {
+        println!("The first argument is the 16 byte hex IV, the second is the message, and the third is the 32 byte hex key. If there is whitespace in the message input, surround in doublequotes.\n\nUsage example: hammeron 5EC4E2032363E027 \"The great forge awaits you!\" 28083A716B0BA8A85F9E0A116FF2EDB7 \n\nNote: don't use the example IV and key, use newly generated ones and never repeat the same IV! The \"anvil\" program can be used to generate a new IV and key.");
+        std::process::exit(1);
+    }
     let siv = args[1].clone();
     let binding = String::from(siv);
     let iv = binding.as_bytes();
