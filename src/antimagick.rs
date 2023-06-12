eb8776eb8264421a3dfc0ca579dfd5dfb33aed01
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-06-11 22:16:47 -0400

    Update antimagick.rs

--- src/antimagick.rs
+++ src/antimagick.rs
@@ -1,14 +1,17 @@
 use hex;
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        println!("Hex decode some data. If there are spaces, surround in double quotes. Usage: antimagick data");
+        return;
+    } 
     if let Some(arg) = std::env::args().nth(1) {
         if let Ok(stro) = arg.parse::<String>() {
             let decoded = hex::decode(stro).unwrap();
             let stringa = std::str::from_utf8(&decoded).unwrap();
             println!("{}", stringa);
-        } else {
-            println!("Invalid argument! Use a single argument.");
-        }
+        } 
     } else {
         println!("No argument provided. If white space is needed, surround the input with doublequotes.\n\nUsage:\n\nantimagick mydata\n\n");
     }
