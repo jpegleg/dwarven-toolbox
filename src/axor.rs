556f00b0a98950986d97f80ef213c72f8a40cc18
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-06-11 21:45:52 -0400

    Update axor.rs

--- src/axor.rs
+++ src/axor.rs
@@ -1,7 +1,7 @@
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 3 {
-        println!("XOR two whole numers and print the resulting hex. Usage: axor num1 num2");
+        println!("XOR two whole numbes (u64) and print the resulting number. Usage: axor num1 num2");
         return;
     }
     if let Some(arg) = std::env::args().nth(1) {
