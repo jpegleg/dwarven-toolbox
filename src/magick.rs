16334b20638e6d7a47579e6a9da2d5645a483a50
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-06-11 22:13:51 -0400

    Update magick.rs

--- src/magick.rs
+++ src/magick.rs
@@ -1,6 +1,11 @@
 use hex;
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        println!("Hex encode some data. If there are spaces, surround in double quotes. Usage: magick data");
+        return;
+    } 
     if let Some(arg) = std::env::args().nth(1) {
         if let Ok(stro) = arg.parse::<String>() {
             let encoded = hex::encode(stro);
