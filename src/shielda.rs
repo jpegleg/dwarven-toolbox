6dee5a1509f99ebb622a1d4c43a6e70189e2b27e
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-06-13 18:34:58 -0400

    Update shielda.rs

--- src/shielda.rs
+++ src/shielda.rs
@@ -1,6 +1,11 @@
 use bs58;
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        println!("Base58 encode some data. If there are spaces, surround in double quotes. Usage: shielda data");
+        return;
+    }
     if let Some(arg) = std::env::args().nth(1) {
         if let Ok(stro) = arg.parse::<String>() {
             let encoded = bs58::encode(stro).into_string();
