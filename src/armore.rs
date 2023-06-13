e0fe400b620794d9e8182099374605e2f2809cc7
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-06-13 18:37:14 -0400

    Update armore.rs

--- src/armore.rs
+++ src/armore.rs
@@ -1,6 +1,11 @@
 use base64::{Engine as _};
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        println!("Base64 encode some data. If there are spaces, surround in double quotes. Usage: aremore mydata");
+        return;
+    }
     if let Some(arg) = std::env::args().nth(1) {
         if let Ok(stro) = arg.parse::<String>() {
             let armor = base64::engine::general_purpose::STANDARD_NO_PAD.encode(stro);
