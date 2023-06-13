7b665674f5cec0f1243ce8ffde5dc8a7c64d78e6
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-06-13 18:35:54 -0400

    Update dshielda.rs

--- src/dshielda.rs
+++ src/dshielda.rs
@@ -1,6 +1,11 @@
 use bs58;
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        println!("Base58 dencode some data. If there are spaces, surround in double quotes. Usage: dshielda base58data");
+        return;
+    }
     if let Some(arg) = std::env::args().nth(1) {
         if let Ok(stro) = arg.parse::<String>() {
             let decoded = bs58::decode(stro).into_vec().unwrap();
