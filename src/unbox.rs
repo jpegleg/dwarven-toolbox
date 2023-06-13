8fe53a9cc9aba36eb0ffb762fab0a9403625f705
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-06-13 16:11:33 -0400

    Create unbox.rs

---
+++ src/unbox.rs
@@ -0,0 +1,31 @@
+use flate2::read::GzDecoder;
+use std::io::prelude::*;
+use hex;
+
+fn hex_decode_gzip(hex_string: &str) -> Result<String, String> {
+    let decoded_bytes = hex::decode(hex_string).map_err(|_| "Failed to decode hex string")?;
+    let mut decoder = GzDecoder::new(&decoded_bytes[..]);
+    let mut decompressed_data = Vec::new();
+    decoder
+        .read_to_end(&mut decompressed_data)
+        .map_err(|_| "Failed to decompress Gzip data")?;
+
+    let hex_encoded = hex::encode(&decompressed_data);
+    Ok(hex_encoded)
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        println!("Gzip decompress some data. If there are spaces, surround in double quotes. Usage: unbox 1f8b08000000000000ff0bc9485548294f2c2a4bcd53c8cd4ccf2851040000b9c2c112000000");
+        return;
+    } 
+    if let Some(arg) = std::env::args().nth(1) {
+        if let Ok(stro) = arg.parse::<String>() {
+            let encoded = hex_decode_gzip(&stro).unwrap();
+            println!("{}", encoded);
+        }
+    } else {
+        println!("No argument provided. The data must be hex encoded gzip data such produced by \"box\".\n\nUsage:\n\nunbox 1f8b08000000000000ff0bc9485548294f2c2a4bcd53c8cd4ccf2851040000b9c2c112000000\n\n");
+    }
+}
