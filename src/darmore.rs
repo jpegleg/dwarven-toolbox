ac052fc29428b1d3091c82e792b3fd04ab949c31
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-06-13 18:38:27 -0400

    Update darmore.rs

--- src/darmore.rs
+++ src/darmore.rs
@@ -2,6 +2,11 @@ use std::str;
 use base64::{Engine as _};
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        println!("Base64 decode some data. If there are spaces, surround in double quotes. Usage: darmore dGhlIGdyZWF0IGR3YXJ2ZW4gZm9yZ2U");
+        return;
+    }
     if let Some(arg) = std::env::args().nth(1) {
         if let Ok(stro) = arg.parse::<String>() {
             let darmor = base64::engine::general_purpose::STANDARD_NO_PAD.decode(stro).unwrap();
