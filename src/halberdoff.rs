df81477a3e6a56b3189525f745849eb4f94d7526
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-06-13 21:45:13 -0400

    Create halberdoff.rs

---
+++ src/halberdoff.rs
@@ -0,0 +1,65 @@
+use chacha20poly1305::aead::{Aead, KeyInit};
+use chacha20poly1305::Key;
+use chacha20poly1305::XNonce;
+
+use zeroize::Zeroize;
+
+fn decrypt_string(nonce: &[u8], cipher_hex: &str, key: &[u8; 32]) -> Result<String, String> {
+    let ciphertext = hex::decode(cipher_hex).map_err(|_| "Failed to decode ciphertext hex")?;
+    let nonce_size = 12;
+    if ciphertext.len() < nonce_size {
+        return Err("Invalid ciphertext".to_string());
+    }
+
+    let (received_nonce, received_ciphertext) = ciphertext.split_at(nonce_size);
+    let nonce = if nonce.is_empty() {
+        received_nonce
+    } else {
+        nonce
+    };
+
+    let key = Key::from_slice(key);
+    let cipher = chacha20poly1305::XChaCha20Poly1305::new(key);
+
+    let plaintext = cipher
+        .decrypt(XNonce::from_slice(nonce), received_ciphertext)
+        .map_err(|_| "Decryption failed")?;
+
+    let plaintext = String::from_utf8(plaintext).map_err(|_| "Invalid UTF-8")?;
+
+    Ok(plaintext)
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 4 {
+        println!("The first argument is the 24 byte hex nonce, the second is the hex encoded ciphertext, and the third is the 32 byte hex key. If there is whitespace in the message input, surround in doublequotes.\n\nUsage example: halberdoff 4450501658F5 3531353139364146344230383934353843354141423330429e2adfd32010e1519776b2abdf9e939af04ae0175af5b09cb1469ce5c035411f1b320fe0f06a6d37152720af85d77a2b3a706ceaededed 09578F03D7F3FBEB0769A7A8AC358936 \n\nNote: don't use the example nonce and key, use newly generated ones and never repeat the same! The \"anvil\" program can be used to generate a new nonce and key.");
+        std::process::exit(1);
+    }
+
+    let smessage = args[2].clone();
+    
+    let nonce = args[1].clone();
+    let binding = String::from(nonce);
+    let snonce = binding.as_bytes();
+    let pos = snonce.len();
+    let mut buffer = [0u8; 24];
+    buffer[..pos].copy_from_slice(snonce);
+
+    let key = args[3].clone();
+    let mut skey = hex::decode(key).expect("Failed to decode provided key!");
+    let mut buffer2 = [0u8; 32];
+    let pos2 = skey.len();
+    buffer2[..pos2].copy_from_slice(&skey);
+    
+    let result = decrypt_string(&mut buffer, &smessage, &mut buffer2);
+    match result {
+        Ok(output) => {
+            println!("{}", output);
+        }
+        Err(error) => {
+            println!("Error: {}", error);
+        }
+    }
+    skey.zeroize();
+}
