0df88d08da80b811d3af687677d5c969cfdbb5ee
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-06-13 21:12:00 -0400

    Update anvil.rs

--- src/anvil.rs
+++ src/anvil.rs
@@ -20,6 +20,23 @@ fn gennonce() -> String {
     hex_chars
 }
 
+fn gennoncex() -> String {
+    let mut rng = StdRng::from_entropy();
+    let hex_chars: String = iter::repeat(())
+        .map(|()| {
+            let char_range = Uniform::from(0..16);
+            let value = match rng.sample(char_range) {
+                0..=9 => (b'0' + rng.sample(Uniform::from(0..10))) as char,
+                10..=15 => (b'A' + rng.sample(Uniform::from(0..6))) as char,
+                _ => unreachable!(),
+            };
+            value
+        })
+        .take(24)
+        .collect();
+    hex_chars
+}
+
 fn geniv() -> String {
     let mut rng = StdRng::from_entropy();
     let hex_chars: String = iter::repeat(())
@@ -61,4 +78,6 @@ fn main() {
     println!("IV: {}", iv);
     let key = genkey();
     println!("KEY: {}", key);
+    let noncex = gennoncex();
+    println!("XNONCE: {}", noncex);
 }
