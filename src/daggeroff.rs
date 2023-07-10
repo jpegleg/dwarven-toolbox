f295be7b44f0e1a001729d7d0ba0134796595f7e
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-07-10 19:41:52 -0400

    Update daggeroff.rs
    adopting BigInt

--- src/daggeroff.rs
+++ src/daggeroff.rs
@@ -1,11 +1,13 @@
+use num_bigint::BigInt;
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 2 {
-        println!("Convert u64 to hex. Usage: daggeroff data");
+        println!("Convert BigInt to hex. Usage: daggeroff data");
         return;
     } 
     if let Some(arg) = std::env::args().nth(1) {
-        if let Ok(stro) = arg.parse::<u64>() {
+        if let Ok(stro) = arg.parse::<BigInt>() {
             println!("{:X}", stro);
         }
     } else {
