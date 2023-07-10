890476df3fe6bf7964ec3d20f45e165cac4d94da
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-07-10 19:32:04 -0400

    Update crossbow.rs
    keeping original f64 crossbow, BigInt multiplication to new program ballista

--- src/crossbow.rs
+++ src/crossbow.rs
@@ -1,26 +1,25 @@
 use std::env;
-use num_bigint::BigInt;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 3 {
-        println!("Usage: crossbbow <number1> <number2>");
+        println!("Usage: crossbow <number1> <number2>");
         return;
     }
 
-    let number1: BigInt = match args[1].parse() {
+    let number1: f64 = match args[1].parse() {
         Ok(num) => num,
         Err(_) => {
-            println!("Error: First argument must be a valid BigInt number");
+            println!("Error: First argument must be a valid f64 number");
             return;
         }
     };
 
-    let number2: BigInt = match args[2].parse() {
+    let number2: f64 = match args[2].parse() {
         Ok(num) => num,
         Err(_) => {
-            println!("Error: Second argument must be a valid BigInt number");
+            println!("Error: Second argument must be a valid f64 number");
             return;
         }
     };
