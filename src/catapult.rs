29ad7ac4ebb1a2da05786ba45533ef16f188ed93
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-07-10 19:35:34 -0400

    Update catapult.rs
    reverting catapult to f64, BigInt version moved to trebuchet

--- src/catapult.rs
+++ src/catapult.rs
@@ -1,5 +1,4 @@
 use std::env;
-use num_bigint::BigInt;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,22 +8,22 @@ fn main() {
         return;
     }
 
-    let number1: BigInt = match args[1].parse() {
+    let number1: f64 = match args[1].parse() {
         Ok(num) => num,
         Err(_) => {
-            println!("Error: First argument must be a valid BigInt number");
+            println!("Error: First argument must be a valid f64 number.");
             return;
         }
     };
 
-    let number2: u32 = match args[2].parse() {
+    let number2: f64 = match args[2].parse() {
         Ok(num) => num,
         Err(_) => {
-            println!("Error: Second argument must be a valid u32 number");
+            println!("Error: Second argument must be a valid f64 number.");
             return;
         }
     };
 
-    let result = number1.pow(number2);
+    let result = number1.powf(number2);
     println!("{}", result);
 }
