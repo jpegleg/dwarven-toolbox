a5a56689aab22e3a7ca32aa2ffa6c0c96e572458
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-07-10 19:47:30 -0400

    Update daggeron.rs
    adopting BigUint

--- src/daggeron.rs
+++ src/daggeron.rs
@@ -1,18 +1,22 @@
 use hex;
+use num_bigint::{BigUint};
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 2 {
-        println!("Convert hex data to a u64 integer. Usage: daggeron data");
+        println!("Convert hex data to a BigUint. Usage: daggeron data");
         return;
     } 
     if let Some(arg) = std::env::args().nth(1) {
         if let Ok(stro) = arg.parse::<String>() {
-            let decoded = hex::decode(stro).unwrap();
-            let mut buf = [0u8; 8];
-            let len = 8.min(decoded.len());
-            buf[..len].copy_from_slice(&decoded[..len]);
-            let rezint = u64::from_le_bytes(buf);
+            let bytes: Vec<u8> = match hex::decode(stro) {
+                Ok(decoded_bytes) => decoded_bytes,
+                Err(_) => {
+                    eprintln!("Failed to decode the string.");
+                    return
+                }
+            };
+            let rezint = BigUint::from_bytes_be(&bytes);
             println!("{}", rezint);
         }
     } else {
