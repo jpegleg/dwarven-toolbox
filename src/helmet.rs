034559a3fe5640c5954a0c8f6631c1db50d59c12
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-07-10 19:55:56 -0400

    Create helmet.rs
    HKDF function

---
+++ src/helmet.rs
@@ -0,0 +1,27 @@
+use std::env;
+use hkdf::Hkdf;
+use sha2::Sha256;
+use hex;
+use zeroize::Zeroize;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 4 {
+        println!("First arg is the input key, the second arg is the salt, and the third arg is the info field for HKDF.\n\nUsage: helmet something 00000001 dangstuffhere");
+        std::process::exit(1);
+    }
+    let ikm = args[1].clone();
+    let skim = String::from(ikm);
+    let bskim = skim.as_bytes();
+    let salt = args[2].clone();
+    let ssalt = String::from(salt);
+    let bssalt = ssalt.as_bytes();
+    let info = args[3].clone();
+    let sinfo = String::from(info);
+    let bsinfo = sinfo.as_bytes();
+    let hk = Hkdf::<Sha256>::new(Some(&bssalt[..]), &bskim);
+    let mut okm = [0u8; 42];
+    hk.expand(&bsinfo, &mut okm).expect("Failed to expand HKDF....");
+    println!("{}", hex::encode(&okm));
+    okm.zeroize();
+}
