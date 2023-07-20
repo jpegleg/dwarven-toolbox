d74ec8ee86c2bcbb2ac16e4a4d36b45a294d9ee2
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-07-20 00:43:59 -0400

    Update forgeoff.rs
    rewrite

--- src/forgeoff.rs
+++ src/forgeoff.rs
@@ -1,83 +1,46 @@
 use std::env;
 use std::fs::File;
-use std::io::{Read, Write};
-use std::path::Path;
-use ed25519_dalek::Keypair;
-
-extern crate base64;
-extern crate ed25519_dalek;
-
+use std::io::{self, Read, Write};
+use blake3::Hasher;
 use chacha20poly1305::{
-    aead::{stream, KeyInit},
+    aead::{AeadInPlace, KeyInit},
     XChaCha20Poly1305,
 };
-use pbkdf2::{pbkdf2_hmac};
-use sha2::Sha256;
-use hex;
-use zeroize::Zeroize;
+use chacha20poly1305::aead::generic_array::GenericArray;
 
-fn main() {
+fn hash_key(key_data: &[u8]) -> [u8; 32] {
+    let mut hasher = Hasher::new();
+    hasher.update(key_data);
+    let hashed_key = hasher.finalize();
+    *hashed_key.as_bytes()
+}
+
+fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 5 {
-        println!("{{\"ERROR\": \"Wrong number of args. The first arg is the file to decrypt, the second arg is the keypair binary file path, as created from makesigil. The third argument is destination file path for the plaintext. The fourth argument is the NONCE, as provided by forgeon etc.\"}}");
-        std::process::exit(1);
-    }
-    let data = args[1].clone();
-    let ciph = args[3].clone();
-    let sdata = String::from(data);
-    let scipher = String::from(ciph);
-    let datafile_path = Path::new(&sdata);
-    let destfile_path = Path::new(&scipher);
-    let _ = File::open(&datafile_path).expect("Failed to open the file");
-    let keyin = args[2].clone();
-    let keyfile_path = Path::new(&keyin);
-    let mut keyfile = File::open(&keyfile_path).expect("Failed to open the file");
-    let mut keybytes = Vec::new();
-    keyfile.read_to_end(&mut keybytes).expect("Failed to read the file");
-    let keypair: Keypair = Keypair::from_bytes(&keybytes).expect("Failed to read bytes to keypair.");
-    let mut private_key_bytes: [u8; 64] = keypair.to_bytes();
-
-    let usitern: u32 = 2100;
-    let ssalt = "e7922c935f16707-H.+";
-    let bssalt = ssalt.as_bytes();
-    let mut keyslot = [0u8; 20];
-    pbkdf2_hmac::<Sha256>(&mut private_key_bytes, bssalt, usitern, &mut keyslot);
-    let hexkey = hex::encode(&keyslot);
-    keyslot.zeroize();
-    let izbhex = hexkey.as_bytes();
-    let bhex = &izbhex[0..32];
-
-    const BUFLEN: usize = 4096;
-    let mut buffer = [0u8; BUFLEN];
-
-    let mut source_file = File::open(datafile_path).expect("Failed to open source file.");
-    let mut destfile = File::create(destfile_path).expect("Failed to open destination file.");
-    let nonce = args[4].clone();
-    let nonbuf = nonce.as_bytes();
-
-    let aead = XChaCha20Poly1305::new(bhex.as_ref().into());
-    let mut streaming = stream::DecryptorBE32::from_aead(aead, nonbuf.as_ref().into());
-
-
-    loop {
-        let read_count = source_file.read(&mut buffer).unwrap();
-
-        if read_count == BUFLEN {
-            let ciphertext = streaming
-                .decrypt_next(buffer.as_slice())
-                .map_err(|_err| "ERROR decrypting large file chunk.");
-            let writeciph = &ciphertext.unwrap();
-            let _ = destfile.write(&writeciph);
-        } else {
-            let ciphertext = streaming
-                .decrypt_last(&buffer[..read_count])
-                .map_err(|_err| "ERROR decrypting end of large file.");
-            let writeciph = &ciphertext.unwrap();
-            let _ = destfile.write(&writeciph);
-            break;
-        }
+    if args.len() != 4 {
+        eprintln!("Usage: {} <ciphertext_file> <key_file> <plaintext_file>", args[0]);
+        return Ok(());
     }
 
-    keybytes.zeroize();
+    let ciphertext_file_path = &args[1];
+    let key_file_path = &args[2];
+    let plaintext_file_path = &args[3];
+    let mut ciphertext_file = File::open(ciphertext_file_path)?;
+    let mut ciphertext = Vec::new();
+    ciphertext_file.read_to_end(&mut ciphertext)?;
+    let mut key_file = File::open(key_file_path)?;
+    let mut key_data = Vec::new();
+    key_file.read_to_end(&mut key_data)?;
+    let hashed_key = hash_key(&key_data);
+    let aead = XChaCha20Poly1305::new(GenericArray::from_slice(&hashed_key));
+    let nonce = chacha20poly1305::XNonce::from_slice(&ciphertext[..24]);
+    let tag = GenericArray::clone_from_slice(&ciphertext[24..40]);
+    let mut plaintext = ciphertext[40..].to_vec();
+    aead.decrypt_in_place_detached(&nonce, &[], &mut plaintext, &tag)
+        .expect("Error: Decryption failed.");
 
+    let mut plaintext_file = File::create(plaintext_file_path)?;
+    plaintext_file.write_all(&plaintext)?;
+    println!("Decryption completed successfully.");
+    Ok(())
 }
