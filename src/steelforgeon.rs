b7dc7d7af679605ad9257e7967d5191ba0d4f90c
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-07-20 21:08:14 -0400

    Create steelforgeon.rs

---
+++ src/steelforgeon.rs
@@ -0,0 +1,50 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, Read, Write};
+use rand::{Rng, SeedableRng};
+use rand::rngs::StdRng;
+use argon2::Argon2;
+use chacha20poly1305::{
+    aead::{AeadInPlace, KeyInit},
+    XChaCha20Poly1305,
+};
+use chacha20poly1305::aead::generic_array::GenericArray;
+
+fn hash_key(password: &[u8], salt: &[u8]) -> [u8; 32] {
+    let mut okm = [0u8; 32];
+    let _ = Argon2::default().hash_password_into(password, salt, &mut okm);
+    okm
+}
+
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        eprintln!("Usage: {} <plaintext_file> <ciphertext_file>", args[0]);
+        return Ok(());
+    }
+
+    let plaintext_file_path = &args[1];
+    let ciphertext_file_path = &args[2];
+    let mut plaintext_file = File::open(plaintext_file_path)?;
+    let mut plaintext = Vec::new();
+    plaintext_file.read_to_end(&mut plaintext)?;
+    let mut rng = StdRng::from_entropy();
+    let mut nonce = [0u8; 24];
+    rng.fill(&mut nonce);
+    let bnon = hex::encode(&nonce);
+    let binding = "00004444".to_owned() + &bnon;
+    let salt = binding.as_bytes();
+    let strpassword = rpassword::prompt_password("Password: ")?;
+    let password = strpassword.as_bytes();
+    let hashed_key = hash_key(&password, &salt);
+    let aead = XChaCha20Poly1305::new(GenericArray::from_slice(&hashed_key));
+    let mut ciphertext_file = File::create(ciphertext_file_path)?;
+    let mut ciphertext = plaintext.to_vec();
+    let tag = aead.encrypt_in_place_detached(&nonce.into(), &[], &mut ciphertext)
+        .expect("Encryption failed.");
+    ciphertext_file.write_all(&nonce)?;
+    ciphertext_file.write_all(&tag)?;
+    ciphertext_file.write_all(&ciphertext)?;
+    println!("Encryption completed successfully.");
+    Ok(())
+}
