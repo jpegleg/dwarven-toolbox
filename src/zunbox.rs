57426176a172cc4f962360bd267e76b3a443dbde
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-07-22 17:18:43 -0400

    Update zunbox.rs
    fixing help messages for zunbox

--- src/zunbox.rs
+++ src/zunbox.rs
@@ -8,7 +8,7 @@ fn hex_decode_gzip(hex_string: &str) -> Result<String, String> {
     let mut decompressed_data = Vec::new();
     decoder
         .read_to_end(&mut decompressed_data)
-        .map_err(|_| "Failed to decompress Gzip data")?;
+        .map_err(|_| "Failed to decompress zlib data")?;
 
     let hex_encoded = hex::encode(&decompressed_data);
     Ok(hex_encoded)
@@ -17,7 +17,7 @@ fn hex_decode_gzip(hex_string: &str) -> Result<String, String> {
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 2 {
-        println!("Gzip decompress some data. If there are spaces, surround in double quotes. Usage: zunbox 789c0bc94855482f4a4d2c5128c82f4f2d52c84f5348294f2c2a4bcd53c8cd4ccf28510400cc640c08");
+        println!("Zlib decompress some data. If there are spaces, surround in double quotes. Usage: zunbox 789c0bc94855482f4a4d2c5128c82f4f2d52c84f5348294f2c2a4bcd53c8cd4ccf28510400cc640c08");
         return;
     } 
     if let Some(arg) = std::env::args().nth(1) {
@@ -26,6 +26,6 @@ fn main() {
             println!("{}", encoded);
         }
     } else {
-        println!("No argument provided. The data must be hex encoded gzip data such produced by \"box\".\n\nUsage:\n\nzunbox 789c0bc94855482f4a4d2c5128c82f4f2d52c84f5348294f2c2a4bcd53c8cd4ccf28510400cc640c08\n\n");
+        println!("No argument provided. The data must be hex encoded zlib data such produced by \"zbox\".\n\nUsage:\n\nzunbox 789c0bc94855482f4a4d2c5128c82f4f2d52c84f5348294f2c2a4bcd53c8cd4ccf28510400cc640c08\n\n");
     }
 }
