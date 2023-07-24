ede5ecceaf703a1aa5c7c786132bbfb118d5e512
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-07-24 02:26:31 -0400

    Update hashkey.rs

--- src/hashkey.rs
+++ src/hashkey.rs
@@ -2,7 +2,7 @@ use argon2::Argon2;
 use blake3::Hasher;
 
 #[allow(dead_code)]
-pub fn hash_key(password: &[u8], salt: &[u8]) -> [u8; 32] {
+pub fn a2(password: &[u8], salt: &[u8]) -> [u8; 32] {
     let mut okm = [0u8; 32];
     let _ = Argon2::default().hash_password_into(password, salt, &mut okm);
     okm
