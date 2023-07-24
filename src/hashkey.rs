e47ca85df2030956967ae72eeda0f6da828562b6
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-07-24 02:11:13 -0400

    Update hashkey.rs

--- src/hashkey.rs
+++ src/hashkey.rs
@@ -1,7 +1,15 @@
 use crate::Argon2;
+use blake3::Hasher;
 
 pub fn hash_key(password: &[u8], salt: &[u8]) -> [u8; 32] {
     let mut okm = [0u8; 32];
     let _ = Argon2::default().hash_password_into(password, salt, &mut okm);
     okm
+}
+
+pub fn b3(key_data: &[u8]) -> [u8; 32] {
+    let mut hasher = Hasher::new();
+    hasher.update(key_data);
+    let hashed_key = hasher.finalize();
+    *hashed_key.as_bytes()
 }
