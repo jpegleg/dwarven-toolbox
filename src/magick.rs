3e4a7839ae73e0dc9ba2ac307d5788eb322e7559
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-07-30 00:03:01 -0400

    Update magick.rs

--- src/magick.rs
+++ src/magick.rs
@@ -15,3 +15,13 @@ fn main() {
         println!("No argument provided. If white space is needed, surround the input with doublequotes.\n\nUsage:\n\nmagick mydata\n\n");
     }
 }
+
+#[test]
+fn test() {
+    let input = "Great power in the encoding...";
+    let hex = "477265617420706f77657220696e2074686520656e636f64696e672e2e2e";
+    let lencoded = hex::encode(input);
+    let binding = lencoded.as_bytes();
+    let encoded = std::str::from_utf8(&binding).unwrap();
+    assert_eq!(hex, encoded);
+}
