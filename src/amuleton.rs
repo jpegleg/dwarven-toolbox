cce56fdd1360b497dc97b81098d75234cd59686f
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-07-29 20:56:52 -0400

    Update amuleton.rs

--- src/amuleton.rs
+++ src/amuleton.rs
@@ -25,3 +25,20 @@ fn main() {
     let signature: Signature = keypair.sign(message);
     println!("Public key: {:?}\nSignature: {}", pub64, signature);
 }
+
+#[test]
+fn test() {
+    let input = "The great and powerful dwarven amulet!";
+    let mut csprng = OsRng{};
+    let keypair: Keypair = Keypair::generate(&mut csprng);
+    let public_key_bytes: [u8; PUBLIC_KEY_LENGTH] = keypair.public.to_bytes();
+    let _ = base64::engine::general_purpose::STANDARD_NO_PAD.encode(&public_key_bytes);
+    let message: &[u8] = input.as_bytes();
+    let signature: Signature = keypair.sign(&message);
+    let mut csprng2 = OsRng{};
+    let keypair2: Keypair = Keypair::generate(&mut csprng2);
+    let public_key_bytes2: [u8; PUBLIC_KEY_LENGTH] = keypair2.public.to_bytes();
+    let _ = base64::engine::general_purpose::STANDARD_NO_PAD.encode(&public_key_bytes2);
+    let signature2: Signature = keypair2.sign(message);
+    assert_ne!(signature2, signature);
+}
