b9bac77607c4d7e35f2475b55640589a9b31ff4e
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-07-30 00:02:07 -0400

    Update antimagick.rs

--- src/antimagick.rs
+++ src/antimagick.rs
@@ -16,3 +16,13 @@ fn main() {
         println!("No argument provided. If white space is needed, surround the input with doublequotes.\n\nUsage:\n\nantimagick mydata\n\n");
     }
 }
+
+#[test]
+fn test() {
+    let input = "Great power in the encoding...";
+    let hex = "477265617420706f77657220696e2074686520656e636f64696e672e2e2e";
+    let lencoded = hex::decode(hex);
+    let binding = lencoded.unwrap();
+    let decoded = std::str::from_utf8(&binding).unwrap();
+    assert_eq!(input, decoded);
+}
