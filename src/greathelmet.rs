8e3b0b51d63893fc047e1ac8850f625fd467b459
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-09-06 21:58:17 -0400

    Update greathelmet.rs
    new version

--- src/greathelmet.rs
+++ src/greathelmet.rs
@@ -10,17 +10,19 @@ fn main() {
         println!("The first argument is a password string, the second is a salt string, and the third is the number of iterations as a u32.\n\nUsage: greathelmet something saltysalt 20\n");
         std::process::exit(1);
     }
-    let pass = args[1].clone();
-    let spass = String::from(pass);
-    let bspass = spass.as_bytes();
-    let salt = args[2].clone();
-    let ssalt = String::from(salt);
-    let bssalt = ssalt.as_bytes();
-    let itern = args[3].clone();
-    let sitern = String::from(itern);
-    let usitern: u32 = sitern.parse().unwrap();
+    let pass = &args[1];
+    let bspass = pass.as_bytes();
+    let salt = &args[2];
+    let bssalt = salt.as_bytes();
+    let itern = &args[3];
+    let usitern: u32 = match itern.parse::<u32>() {
+        Ok(_) => itern.parse::<u32>().expect("failed to parse u32"),
+        _ => {
+            eprintln!("Failed to parse u32.");
+            return
+        }
+    };
     let mut keyslot = [0u8; 20];
-
     pbkdf2_hmac::<Sha256>(bspass, bssalt, usitern, &mut keyslot);
     println!("{}", hex::encode(&keyslot));
     keyslot.zeroize();
