e194c7a3c972a8117fbf9743a576bafd71371db2
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-09-06 21:36:18 -0400

    Update flip.rs
    new version

--- src/flip.rs
+++ src/flip.rs
@@ -3,15 +3,10 @@ fn main() {
     if args.len() != 2 {
         println!("Reverse a string. Usage: flip data");
         return;
-    } 
-    if let Some(arg) = std::env::args().nth(1) {
-        if let Ok(stri) = arg.parse::<String>() {
-            let strv = &mut stri.to_string().into_bytes();
-            strv.reverse();
-            let rezint = std::str::from_utf8(strv).unwrap().to_string();
-            println!("{}", rezint);
-        }
-    } else {
-        println!("No argument provided. \n\nUsage:\n\nflip data\n\n");
     }
+    let stri = &args[1];
+    let strv = &mut stri.to_string().into_bytes();
+    strv.reverse();
+    let rezint = std::str::from_utf8(strv).unwrap().to_string();
+    println!("{}", rezint);
 }
