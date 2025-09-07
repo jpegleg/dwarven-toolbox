e8812f75fb1ec193e3562c57f55788a94aa88ece
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-09-06 22:06:25 -0400

    Update magick.rs
    new version

--- src/magick.rs
+++ src/magick.rs
@@ -5,23 +5,8 @@ fn main() {
     if args.len() != 2 {
         println!("Hex encode some data. If there are spaces, surround in double quotes. Usage: magick data");
         return;
-    } 
-    if let Some(arg) = std::env::args().nth(1) {
-        if let Ok(stro) = arg.parse::<String>() {
-            let encoded = hex::encode(stro);
-            println!("{}", encoded);
-        }
-    } else {
-        println!("No argument provided. If white space is needed, surround the input with doublequotes.\n\nUsage:\n\nmagick mydata\n\n");
     }
-}
-
-#[test]
-fn test() {
-    let input = "Great power in the encoding...";
-    let hex = "477265617420706f77657220696e2074686520656e636f64696e672e2e2e";
-    let lencoded = hex::encode(input);
-    let binding = lencoded.as_bytes();
-    let encoded = std::str::from_utf8(&binding).unwrap();
-    assert_eq!(hex, encoded);
+    let stro = &args[1];
+    let encoded = hex::encode(stro);
+    println!("{}", encoded);
 }
