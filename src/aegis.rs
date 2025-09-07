1eaaeb7a8ad96ffaba7a1af788b79f1c3fc17fc1
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-09-06 20:40:55 -0400

    Update aegis.rs
    new version

--- src/aegis.rs
+++ src/aegis.rs
@@ -1,26 +1,15 @@
-use rand::{Rng, SeedableRng};
-use rand::rngs::StdRng;
-use rand::distributions::Uniform;
-use std::iter;
+use zeroize::Zeroize;
+
+#[path = "./generate.rs"]
+mod generate;
+use generate::hexgen;
 
 fn genkey512() -> String {
-    let mut rng = StdRng::from_entropy();
-    let hex_chars: String = iter::repeat(())
-        .map(|()| {
-            let char_range = Uniform::from(0..16);
-            let value = match rng.sample(char_range) {
-                0..=9 => (b'0' + rng.sample(Uniform::from(0..10))) as char,
-                10..=15 => (b'A' + rng.sample(Uniform::from(0..6))) as char,
-                _ => unreachable!(),
-            };
-            value
-        })
-        .take(64)
-        .collect();
-    hex_chars
+  hexgen(64)
 }
 
 fn main() {
-    let key = genkey512();
+    let mut key = genkey512();
     println!("{key}");
+    key.zeroize();
 }
