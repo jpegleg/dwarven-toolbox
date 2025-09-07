a0427dd4b1617cebac33df554b0cbed5a90af33d
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-09-06 21:38:54 -0400

    Update gaxor.rs
    new version

--- src/gaxor.rs
+++ src/gaxor.rs
@@ -6,18 +6,16 @@ fn main() {
         println!("XOR two whole numbers (BigInt) and print the resulting number. Usage: gaxor num1 num2");
         return;
     }
-    if let Some(arg) = std::env::args().nth(1) {
-        if let Ok(first) = arg.parse::<BigInt>() {
-            if let Some(arg2) = std::env::args().nth(2) {
-                if let Ok(second) = arg2.parse::<BigInt>() {
-                    let axor = first ^ second;
-                    println!("{}", axor);
-                } else {
-                    println!("Use two whole (BigInt) numbers for the arguments.");
-                }
-            } else {
-                println!("Use two whole (BigInt) numbers for the arguments.");
-            }
-        }
+    let first = &args[1];
+    let second = &args[2];
+    if let Ok(first) = first.parse::<BigInt>() {
+       if let Ok(second) = second.parse::<BigInt>() {
+           let axor = first ^ second;
+           println!("{}", axor);
+       } else {
+           println!("Use two whole (BigInt) numbers for the arguments.");
+       }
+    } else {
+        println!("Use two whole (BigInt) numbers for the arguments.");
     }
 }
