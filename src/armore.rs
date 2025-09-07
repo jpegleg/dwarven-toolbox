e339b9324e4c5489278fc3081728a1272c95b334
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-09-06 20:46:54 -0400

    Update armore.rs
    new version

--- src/armore.rs
+++ src/armore.rs
@@ -6,14 +6,7 @@ fn main() {
         println!("Base64 encode some data. If there are spaces, surround in double quotes. Usage: aremore mydata");
         return;
     }
-    if let Some(arg) = std::env::args().nth(1) {
-        if let Ok(stro) = arg.parse::<String>() {
-            let armor = base64::engine::general_purpose::STANDARD_NO_PAD.encode(stro);
-            println!("{}", armor);
-        } else {
-            println!("Invalid argument! Use a single string argument.");
-        }
-    } else {
-        println!("No argument provided. If white space is needed, surround the input with doublequotes.\n\nUsage:\n\narmore mydata\n\n");
-    }
+    let stro = &args[1];
+    let armor = base64::engine::general_purpose::STANDARD_NO_PAD.encode(stro);
+    println!("{}", armor);
 }
