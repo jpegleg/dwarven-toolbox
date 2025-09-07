682facc7a35e9a12f41c36ca55d7cff853e85397
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-09-06 22:17:04 -0400

    Update shielda.rs
    new version

--- src/shielda.rs
+++ src/shielda.rs
@@ -6,14 +6,7 @@ fn main() {
         println!("Base58 encode some data. If there are spaces, surround in double quotes. Usage: shielda data");
         return;
     }
-    if let Some(arg) = std::env::args().nth(1) {
-        if let Ok(stro) = arg.parse::<String>() {
-            let encoded = bs58::encode(stro).into_string();
-            println!("{}", encoded);
-        } else {
-            println!("Invalid argument! Use a single argument.");
-        }
-    } else {
-        println!("No argument provided. If white space is needed, surround the input with doublequotes.\n\nUsage:\n\nshielda mydata\n\n");
-    }
+    let stro = &args[1];
+    let encoded = bs58::encode(stro).into_string();
+    println!("{}", encoded);
 }
