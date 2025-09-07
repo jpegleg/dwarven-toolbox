9ab022e9f6b695cc276c512dc58eb6c2c0c7fc88
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-09-06 22:07:56 -0400

    Update makesigil.rs
    new version - replacing 25519 key file with random file from CSPRNG of the same size

--- src/makesigil.rs
+++ src/makesigil.rs
@@ -1,42 +1,28 @@
-use std::env;
-use std::fs::File;
+use std::error::Error;
 use std::io::Write;
-use std::path::Path;
-use std::os::unix::fs::PermissionsExt;
-use std::fs::set_permissions;
+use std::fs::File;
+use rand::TryRngCore;
 use rand::rngs::OsRng;
-use base64::{Engine as _};
-use ed25519_dalek::{Keypair, PUBLIC_KEY_LENGTH};
 
-extern crate base64;
-extern crate ed25519_dalek;
+fn randombytes(x: &mut [u8], len: usize) {
+    OsRng.try_fill_bytes(&mut x[..len]).expect("OS failed to provide entropy")
+}
 
-use zeroize::Zeroize;
+fn gen_entropy(entropy_size: usize, file_path: &str) -> Result<(), Box<dyn Error>> {
+    let mut init_seed = [0u8; 32];
+    let mut entropy_file = File::create(file_path)?;
+    randombytes(&mut init_seed, 32);
+    entropy_file.write_all(&init_seed)?;
+    entropy_file.set_len(entropy_size.try_into().unwrap())?;
+    Ok(())
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = std::env::args().collect();
     if args.len() != 2 {
-        println!("{{\"ERROR\": \"Wrong number of args. The only arg is a file path write a new ed25519 keypair binary to.\"}}");
-        std::process::exit(1);
+        println!("Write a 256-bit key file. The only arg is the file path.");
+        return;
     }
-    let data = args[1].clone();
-    let sdata = String::from(data);
-    let datafile_path = Path::new(&sdata);
-    let mut datafile = File::create(&datafile_path).expect("Failed to open the file");
-    let mut databytes = Vec::new();
-    let mut csprng = OsRng{};
-    let keypair: Keypair = Keypair::generate(&mut csprng);
-    let secret_key_bytes: Vec<u8> = keypair.secret.to_bytes().to_vec();
-    let public_key_bytes: Vec<u8> = keypair.public.to_bytes().to_vec();
-    databytes.extend_from_slice(&secret_key_bytes);
-    databytes.extend_from_slice(&public_key_bytes);
-
-    let public_key_bytes: [u8; PUBLIC_KEY_LENGTH] = keypair.public.to_bytes();
-    let pub64 = base64::engine::general_purpose::STANDARD_NO_PAD.encode(public_key_bytes);
-    set_permissions(&datafile_path, PermissionsExt::from_mode(0o600)).unwrap();
-    datafile.write_all(&databytes).expect("Failed to read the file");
-
-    println!("Public key: {:?}\nKeypair binary created at: {:?}", pub64, datafile_path);
-    databytes.zeroize();
-
+    let file_path = &args[1];
+    let _ = gen_entropy(32, file_path);
 }
