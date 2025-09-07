97abe2d5e80a01890ccb2dc3a474455c4d492791
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-09-06 22:23:46 -0400

    Update zbox.rs
    new version

--- src/zbox.rs
+++ src/zbox.rs
@@ -8,17 +8,12 @@ fn main() {
     if args.len() != 2 {
         println!("Zlib compress some data. If there are spaces, surround in double quotes. Usage: zbox data");
         return;
-    } 
-    if let Some(arg) = std::env::args().nth(1) {
-        if let Ok(stro) = arg.parse::<String>() {
-            let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
-            let rawdata = stro.as_bytes();
-            let _self = encoder.write_all(rawdata);
-            let gzdata = encoder.finish().unwrap();
-            let encoded = hex::encode(gzdata);
-            println!("{}", encoded);
-        }
-    } else {
-        println!("No argument provided. If white space is needed, surround the input with doublequotes.\n\nUsage:\n\nzbox mydata\n\n");
     }
+    let stro = &args[1];
+    let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
+    let rawdata = stro.as_bytes();
+    let _self = encoder.write_all(rawdata);
+    let gzdata = encoder.finish().unwrap();
+    let encoded = hex::encode(gzdata);
+    println!("{}", encoded);
 }
