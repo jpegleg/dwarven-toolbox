5b3a9a65da5e82273565a0304dcc6b6a52eaa291
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-09-06 21:16:32 -0400

    Update daggeron.rs
    new version

--- src/daggeron.rs
+++ src/daggeron.rs
@@ -6,20 +6,15 @@ fn main() {
     if args.len() != 2 {
         println!("Convert hex data to a BigUint. Usage: daggeron data");
         return;
-    } 
-    if let Some(arg) = std::env::args().nth(1) {
-        if let Ok(stro) = arg.parse::<String>() {
-            let bytes: Vec<u8> = match hex::decode(stro) {
-                Ok(decoded_bytes) => decoded_bytes,
-                Err(_) => {
-                    eprintln!("Failed to decode the string.");
-                    return
-                }
-            };
-            let rezint = BigUint::from_bytes_be(&bytes);
-            println!("{}", rezint);
-        }
-    } else {
-        println!("No argument provided. \n\nUsage:\n\ndaggeron hexdata\n\n");
     }
+    let stro = &args[1];
+    let bytes: Vec<u8> = match hex::decode(stro) {
+        Ok(decoded_bytes) => decoded_bytes,
+        Err(_) => {
+            eprintln!("Failed to decode the string.");
+            return
+        }
+    };
+    let rezint = BigUint::from_bytes_be(&bytes);
+    println!("{}", rezint);
 }
