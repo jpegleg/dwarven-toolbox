a9a78cc7a3f1bb028fdcbd6f031a2e544fae6aba
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-09-07 02:49:25 -0400

    Update generate.rs
    expand generate functions with utfgen

--- src/generate.rs
+++ src/generate.rs
@@ -2,9 +2,18 @@ use rand::Rng;
 use std::iter;
 
 const CHARSET: &[u8] = b"ABCDEF0123456789";
+const UTFSET: &[u8] = b"1!2@3#4$5%6^7&8*9(0)-_=+qazwsxedcrfvtgbyhnujmik,ol.p;/[']QAZWSXEDCRFVTGBYHNUJMIK<OL>P:?{}";
 
+#[allow(dead_code)]
 pub fn hexgen(len: usize) -> String {
     let mut rng = rand::rng();
     let one_char = || CHARSET[rng.random_range(0..CHARSET.len())] as char;
+    iter::repeat_with(one_char).take(len).collect()
+}
+
+#[allow(dead_code)]
+pub fn utfgen(len: usize) -> String {
+    let mut rng = rand::rng();
+    let one_char = || UTFSET[rng.random_range(0..UTFSET.len())] as char;
     iter::repeat_with(one_char).take(len).collect()
 }
