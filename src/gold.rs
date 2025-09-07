a41cf913fc74fed7a8e132b2122f1b1bef1a53e2
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-09-06 21:39:16 -0400

    Update gold.rs
    new version

--- src/gold.rs
+++ src/gold.rs
@@ -1,22 +1,11 @@
-use rand::{Rng, SeedableRng};
-use rand::rngs::StdRng;
-use rand::distributions::Uniform;
-use std::iter;
 use zeroize::Zeroize;
 
-fn gold() {
-    let mut rng = StdRng::from_entropy();
-    let mut goldchar: String = iter::repeat(())
-        .map(|()| {
-            let char_range = Uniform::from(32..127);
-            rng.sample(char_range) as u8 as char
-        })
-        .take(96)
-        .collect();
-    println!("{}", &goldchar);
-    goldchar.zeroize();
-}
+#[path = "./generate.rs"]
+mod generate;
+use generate::hexgen;
 
 fn main() {
-    gold();
+    let mut goldchar = hexgen(96);
+    println!("{}", &goldchar);
+    goldchar.zeroize();
 }
